cad44045d7ef46131cc597ed10e290aa6cd57093
neozhu <new163@163.com>
2015-02-25 18:19:33 +0800

    UDPATE ADD ONE TO MANY

---
+++ CustomBasicScaffolder/Demo/WebApp/Models/Order.cs
@@ -0,0 +1,23 @@
+﻿using Repository.Pattern.Ef6;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public partial class Order:Entity
+    {
+        public Order() {
+            OrderDetails = new HashSet<OrderDetail>();
+        }
+        [Key]
+        public int Id { get; set; }
+        public string Customer { get; set; }
+        public string ShippingAddress { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+}
---
+++ CustomBasicScaffolder/Demo/WebApp/Repositories/Orders/OrderRepository.cs
@@ -0,0 +1,30 @@
+﻿
+                    
+      
+
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+using Repository.Pattern.Repositories;
+
+using WebApp.Models;
+
+namespace WebApp.Repositories
+{
+  public static class OrderRepository  
+    {
+ 
+        
+                public static IEnumerable<OrderDetail>   GetOrderDetails (this IRepositoryAsync<Order> repository,int id)
+        {
+            return repository.Query(n => n.Id == id).Include(x => x.OrderDetails).Select(x => x.OrderDetails).FirstOrDefault();
+        }
+         
+	}
+}
+
+
+
