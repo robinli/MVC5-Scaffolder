c92a1cc3222020d8b46ededea1ea2856b726e050
neozhu <new163@163.com>
2015-12-31 14:38:20 +0800

    update

--- CustomBasicScaffolder/Demo/WebApp/Scripts/inspinia.js
+++ CustomBasicScaffolder/Demo/WebApp/Scripts/inspinia.js
@@ -92,7 +92,7 @@ $(document).ready(function () {
 
     // Append config box / Only for demo purpose
     // Uncomment on server mode to enable XHR calls
-    $.get("skin-config.html", function (data) {
+    $.get("/skin-config.html", function (data) {
         if (!$('body').hasClass('no-skin-config'))
             $('body').append(data);
     });
