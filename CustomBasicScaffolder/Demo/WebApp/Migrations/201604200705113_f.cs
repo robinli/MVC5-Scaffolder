6cf8c5464e2e132f2bce9d6feb94acdc838a6507
neozhu <new163@163.com>
2016-04-20 15:22:05 +0800

    fix

---
+++ CustomBasicScaffolder/Demo/WebApp/Migrations/201604200705113_f.cs
@@ -0,0 +1,16 @@
+namespace WebApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class f : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
