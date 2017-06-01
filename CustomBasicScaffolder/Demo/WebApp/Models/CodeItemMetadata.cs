ee6e5688cc0d5159f39e8014e185f65ff4198561
neozhu <new163@163.com>
2017-06-01 17:20:05 +0800

    fix

---
+++ CustomBasicScaffolder/Demo/WebApp/Models/CodeItemMetadata.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace WebApp.Models
+{
+	[MetadataType(typeof(CodeItemMetadata))]
+	public partial class CodeItem
+	{
+	}
+
+	public partial class CodeItemMetadata
+	{
+		[Display(Name = "BaseCode")]
+		public BaseCode BaseCode { get; set; }
+
+		[Required(ErrorMessage = "Please enter : Id")]
+		[Display(Name = "Id")]
+		public int Id { get; set; }
+
+		[Required(ErrorMessage = "Please enter : Code")]
+		[Display(Name = "值")]
+		[MaxLength(20)]
+		public string Code { get; set; }
+
+		[Required(ErrorMessage = "Please enter : Text")]
+		[Display(Name = "显示名称")]
+		[MaxLength(50)]
+		public string Text { get; set; }
+
+		[Display(Name = "代码类别名称")]
+		public int BaseCodeId { get; set; }
+
+	}
+
+
+
+
+	public class CodeItemChangeViewModel
+	{
+		public IEnumerable<CodeItem> inserted { get; set; }
+		public IEnumerable<CodeItem> deleted { get; set; }
+		public IEnumerable<CodeItem> updated { get; set; }
+	}
+
+}
