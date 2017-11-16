6eebcd2ea3c985778761ea755d03019f637c655f
neozhu <new163@163.com>
2017-11-16 08:39:07 +0800

    update

--- CustomBasicScaffolder/Demo/Source/SqlHelper2/IDatabase.cs
+++ CustomBasicScaffolder/Demo/Source/SqlHelper2/IDatabase.cs
@@ -21,13 +21,13 @@ namespace SqlHelper2 {
         void BulkCopy(DataTable table, int batchSize = 100);
 
         bool HasRow(string sql, object parameters = null);
-        [System.Obsolete("void  void ExecuteDataSet(string sql, object parameters,Action<DataSet> action)", true)]
+        [System.Obsolete("Use void  void ExecuteDataSet(string sql, object parameters,Action<DataSet> action)", true)]
         DataSet ExecuteDataSet(string sql, object parameters);
         void ExecuteDataSet(string sql, object parameters,Action<DataSet> action);
-        [System.Obsolete("void  ExecuteDataTable(string sql, object parameters,Action<DataTable> action)", true)]
+        [System.Obsolete("Use void  ExecuteDataTable(string sql, object parameters,Action<DataTable> action)", true)]
         DataTable ExecuteDataTable(string sql, object parameters=null);
         void ExecuteDataTable(string sql, object parameters,Action<DataTable> action);
-        [System.Obsolete("void ExecuteSpDataSet(string procedureName, object parameters,Action<DataSet> action)", true)]
+        [System.Obsolete("Use void ExecuteSpDataSet(string procedureName, object parameters,Action<DataSet> action)", true)]
         DataSet ExecuteSpDataSet(string procedureName, object parameters);
         void ExecuteSpDataSet(string procedureName, object parameters,Action<DataSet> action);
     }
