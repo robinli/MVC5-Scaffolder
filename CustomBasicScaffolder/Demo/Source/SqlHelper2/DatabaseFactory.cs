0e71d0f054bbff7fc788fc878b9138897f23a3e7
neozhu <new163@163.com>
2018-06-12 22:04:14 +0800

    delete all

--- CustomBasicScaffolder/Demo/Source/SqlHelper2/DatabaseFactory.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SqlHelper2 {
-    public class DatabaseFactory {
-        public static IDatabaseAsync CreateDatabase(string connectionStringName = "DefaultConnection") {
-            return new ConnectionDatabase(connectionStringName);
-        }
-    }
-}
--- CustomBasicScaffolder/Demo/WebApp/Scripts/jquery.dateFormat.js
+++
@@ -1,5 +0,0 @@
-﻿// require dateFormat.js
-// please check `dist/jquery.dateFormat.js` for a complete version
-(function ($) {
-    $.format = DateFormat.format;
-}(jQuery));
